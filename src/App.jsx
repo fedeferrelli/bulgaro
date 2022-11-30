685718caaedd48ce6719887d1a4118499afbb856
Fede <ferrelli.mazza@gmail.com>
2022-11-30 20:59:03 -0300

    Try HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect} from 'react'
 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 import { fetchData } from './api';
 

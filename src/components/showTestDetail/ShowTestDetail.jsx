e64d7d9ac94978a406afdb2d6764ae7894c7850b
Fede <ferrelli.mazza@gmail.com>
2022-11-30 22:07:13 -0300

    Restyle ShowTestDetail

--- src/components/showTestDetail/ShowTestDetail.jsx
+++ src/components/showTestDetail/ShowTestDetail.jsx
@@ -1,4 +1,57 @@
 import { useState, useEffect } from "react";
+import {useNavigate, useParams} from 'react-router-dom'
+
+
+function ShowTestDetail({data}) {
+
+  const { dish } = useParams();
+  const [selectedDish, setSelectedDish] = useState([]);
+
+  useEffect(() => {
+    
+    const setDish = () => {
+      setSelectedDish(data.find(plato=>plato.plato===dish));
+    };
+    typeof data !== "undefined" && setDish();
+  }, [data]);
+
+    
+    const navigate = useNavigate()
+
+    const handlePrice = (string) =>{
+      return '$'+Number(string).toLocaleString('de-DE')
+  }
+
+
+
+    return (
+      <section className="pb-4">
+
+
+      <div className="w-full ">
+      
+      <div className="">
+      <img className="" src={selectedDish?.image}
+        alt={`imagen para ${selectedDish?.title}`} ></img> </div>    
+      <div className="p-2 w-full">  
+      <h1 className="text-2xl text-left capitalize font-semibold text-gray-600"> {selectedDish.plato}</h1>
+      <div 
+      className="flex justify-left"><div 
+      className="rounded-full bg-gray-200 w-auto text-gray-400 px-2 py-1 text-sm"> {selectedDish.categoria} </div>  </div>    
+
+      <h1 className="text-2xl font-bold text-left capitalize text-gray-600 mt-3"> {handlePrice(selectedDish.precio)}</h1>
+         
+      <p className="text-left text-gray-500 mt-2 text-md max-w-prose"> {selectedDish.descripcion}</p>
+      </div>
+
+      </div>
+      <button onClick={()=>navigate(-1)} className="p-4 rounded-full bg-gray-200 shadow-xl text-center text-gray-800 right-2 bottom-2 fixed border border-gray-300/50">Go Back</button>
+      </section>
+  )
+    }
+  
+  
+  /* import { useState, useEffect } from "react";
 
 import { useParams } from "react-router-dom";
 
@@ -34,5 +87,6 @@ console.log(dataToShow)
 );
 
 }
-
+*/
 export default ShowTestDetail;
+ 
